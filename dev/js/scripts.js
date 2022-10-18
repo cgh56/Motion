e3490e97d338601384efdd40596293bfffcee9ec
cgh56 <102631608+cgh56@users.noreply.github.com>
2022-10-18 13:13:51 -0400

    new

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,16 +1,61 @@
-import $ from "jquery";
+import { gsap } from "gsap";
+gsap.set("#trails-btn i",{transformOrigin:"center bottom"});
 
-let $nav = $('nav[data-nav]');
-let isVisible = false;
+function heroAimartion(){
+    var tl= gsap.timeline();
+    tl.from("#first-line",{duration: 1, alpha:0, y:-100})
+        .from("#second-line",{duration: 1, alpha:0, y:-70, delay:0.5})
+        .from("#trails-btn",{duration: 1, alpha:0, y:50, delay:0.75})
+        .from("#trails-btn i",{duration: 1, alpha:0, rotation:180, delay:0.75});
 
-$(".nav-btns").on("click", function(){
-    console.log("click");
-    if(isVisible === false){
-        $nav.show();
-        isVisible = true;
-    }
-    else{
-        $nav.hide();
-        isVisible = false;
-    }
+    return tl;
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(heroAimartion());
+
+var heroSizeNumber = 1;
+
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 768px)", () => {
+    //desktop setup code here...
+    heroSizeNumber = 2;
+});
+
+mm.add("(min-width: 767px)", () => {
+    //mobile setup code here...
+    heroSizeNumber = 1.25;
+});
+
+
+
+gsap.from("#first-line",{duration: 1, alpha:0, y:-100});
+gsap.from("#second-line",{duration: 1, alpha:0, y:-70, delay:0.5});
+gsap.from("#trails-btn",{duration: 1, alpha:0, y:50, delay:0.75});
+gsap.from("#trails-btn i",{duration: 1, alpha:0, rotation:180, delay:0.75});
+
+let trailBtn = document.querySelector("#trails-btn");
+
+var buttonAnimation = gsap.timeline({paused:true});
+buttonAnimation.to("#trails-btn",{duration:.25, scale:heroSizeNumber},"goAway")
+    .to("#trails-btn i",{duration: .25, rotateY:180})
+    .to("#first-line",{duration: .25, alpha:0, y:50},"goAway")
+    .to("#second-line",{duration: .25, alpha:0, y:20},"goAway");
+
+trailBtn.addEventListener("mouseover",function(){
+    buttonAnimation.play();
+
 })
+
+trailBtn.addEventListener("mouseout",function(){
+    buttonAnimation.reverse();
+    // gsap.to("#trails-btn",{duration:1, scale:1});
+
+    // gsap.to("#trails-btn i",{duration: 1, rotateY:0});
+
+    // gsap.to("#first-line",{duration: 1, alpha:1, y:0});
+    // gsap.to("#second-line",{duration: 1, alpha:1, y:0});
+})
+
+
